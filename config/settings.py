c035646395bb47edcdce974d92227298b3f94a40
Nikita Armbrister <nikritus@gmail.com>
2026-07-08 14:11:13 +0700

    refactor: добалвение названия осноной модели по умолчанию

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    MODEL_NAME: str = Field(title='Название модели')
+    MODEL_NAME: str = Field(title='Название модели', default='tencent/hy3:free')
     MODEL_API_KEY: str = Field(title='API ключ к модели с OpenRouter')
     MODEL_API_BASE: str = Field(title='Адрес API OpenRouter', default='https://openrouter.ai/api/v1')
     TEMPERATURE: float = Field(default=0, title='Температура модели')
